85e8745dd99443340d1f9f18a455a5d040674864
Juan Duarte <juan.devcontact@gmail.com>
2023-01-09 23:20:45 -0500

    Ajustes Modo Oscuro, Preferencias del Sistema

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,57 @@
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
 
     eventListeners();
 
     darkMode();
 });
 
-function darkMode () {
+function darkMode() {
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme-dark)');
+    if (prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function () {
+        if (prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    //console.log(prefiereDarkMode.matches);
+    //Boton DarkMode
     const botonDarkMode = document.querySelector('.dark-mode-boton');
     botonDarkMode.addEventListener('click', function () {
         document.body.classList.toggle('dark-mode');
-    })
+
+        //Para que el modo elegido se quede guardado en local-storage
+        if (document.body.classList.contains('dark-mode')) {
+            localStorage.setItem('modo-oscuro', 'true');
+        } else {
+            localStorage.setItem('modo-oscuro', 'false');
+        }
+    });
+
+    //Obtenemos el modo del color actual
+    if (localStorage.getItem('modo-oscuro') === 'true') {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
 }
 
-function eventListeners () {
+
+function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
 
-    mobileMenu.addEventListener('click', navegacionResponsive );
+    mobileMenu.addEventListener('click', navegacionResponsive);
 }
 
-function navegacionResponsive () {
+function navegacionResponsive() {
     const navegacion = document.querySelector('.navegacion');
 
     if (navegacion.classList.contains('mostrar')) {
