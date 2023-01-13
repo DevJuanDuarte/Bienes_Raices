a010724f376d113c6710cd9d82477736d93d31e1
Juan Duarte <juan.devcontact@gmail.com>
2023-01-13 12:11:01 -0500

    Conectando la base de datos

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,4 +1,9 @@
 <?php
+    //Base de datos:
+    require '../../includes/config/database.php';
+
+    $db = conectarDB();
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
