5c24b957ea798fd547283897fd549b8710c3a010
Juan Duarte <juan.devcontact@gmail.com>
2023-01-14 12:01:16 -0500

    Primera Propiedad BD

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -5,14 +5,28 @@
     $db = conectarDB();
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        echo "<pre>";
-        var_dump($_POST);
-        echo "</pre>";
+        // echo "<pre>";
+        // var_dump($_POST);
+        // echo "</pre>";
 
         $titulo = $_POST ['titulo'];
         $precio = $_POST ['precio'];
+        $descripcion = $_POST ['descripcion'];
+        $habitaciones = $_POST ['habitaciones'];
+        $wc = $_POST ['wc'];
+        $estacionamiento = $_POST ['estacionamiento'];
+        $vendedorId = $_POST ['vendedorId'];
 
+        //Insertar en la base de datos:
+        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorId)
+        VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId')";
+        // echo $query;
 
+        $resultado = mysqli_query($db, $query);
+
+        if ($resultado) {
+            echo "Se añadio correctamente";
+        }        
     }
 
     require '../../includes/funciones.php';
@@ -39,7 +53,7 @@
                 <input type="file" id="imagen" accept="image/jpeg, image/png">
 
                 <label for="descripcion">Descripcion</label>
-                <textarea name="" id="" cols="30" rows="10"></textarea>
+                <textarea id="descripcion" name="descripcion"></textarea>
             </fieldset>
 
 
@@ -48,24 +62,24 @@
 
 
                 <label for="habitaciones">Habitaciones</label>
-                <input type="number" id="habitaciones" placeholder="Ejemplo: 3" min="1" max="9">
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ejemplo: 3" min="1" max="9">
 
                 <label for="wc">Baños</label>
-                <input type="number" id="wc" placeholder="Ejemplo: 3" min="1" max="9">
+                <input type="number" id="wc" name="wc" placeholder="Ejemplo: 3" min="1" max="9">
 
                 <label for="estacionamiento">Estacionamiento</label>
-                <input type="number" id="estacionamiento" placeholder="Ejemplo: 3" min="1" max="9">
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ejemplo: 3" min="1" max="9">
             </fieldset>
 
 
             <fieldset>
                 <legend>Vendedor</legend>
-
-                <select>
+ 
+                <select name="vendedorId">
                     <option value="1">Juan</option>
-                    <option value="2">Stefany</option>
+                    <option value="2">David</option>
                 </select>
-            </fieldset>
+            </fieldset> 
 
 
             <input type="submit" value="Crear Propiedad" class="boton boton-verde">
--- nosotros.php
+++ nosotros.php
@@ -1,70 +1,70 @@
 <?php
-    require 'includes/funciones.php';
-    incluirTemplate('header');
+require 'includes/funciones.php';
+incluirTemplate('header');
 ?>
 
-    <main class="contenedido seccion">
-        <h1>Más sobre Nosotros</h1>
-        <div class="contenido-nosotros">
-            <div class="imagen">
-                <picture>
-                    <source srcset="build/img/nosotros.webp" type="image/webp">
-                    <source srcset="build/img/nosotros.jpg" type="image/jpeg">
-                    <img src="build/img/nosotros.jpg" alt="Imagen sobre nosotros" loading="lazy">
-                </picture>
-            </div>
-            <div class="texto-nosotros">
-                <blockquote>
-                    25 Años de experiencia
-                </blockquote>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, maxime repudiandae laudantium
-                    commodi, ratione reiciendis earum praesentium voluptate dicta a quia ipsum dolorem laboriosam
-                    dolorum iure incidunt. Veritatis, quidem perspiciatis.
-                </p>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aspernatur laborum ipsam, eum expedita
-                    recusandae esse suscipit possimus ipsa quo velit, incidunt ratione, perspiciatis iusto dolorum
-                    tenetur sunt delectus quibusdam.
-                </p>
-            </div>
+<main class="contenedido seccion">
+    <h1>Más sobre Nosotros</h1>
+    <div class="contenido-nosotros">
+        <div class="imagen">
+            <picture>
+                <source srcset="build/img/nosotros.webp" type="image/webp">
+                <source srcset="build/img/nosotros.jpg" type="image/jpeg">
+                <img src="build/img/nosotros.jpg" alt="Imagen sobre nosotros" loading="lazy">
+            </picture>
         </div>
-    </main>
+        <div class="texto-nosotros">
+            <blockquote>
+                25 Años de experiencia
+            </blockquote>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, maxime repudiandae laudantium
+                commodi, ratione reiciendis earum praesentium voluptate dicta a quia ipsum dolorem laboriosam
+                dolorum iure incidunt. Veritatis, quidem perspiciatis.
+            </p>
 
-    <section class="contenedor seccion">
-        <h1>Más sobre nosotros</h1>
-        <div class="iconos-nosotros">
-            <div class="icono">
-                <img src="build/img/icono1.svg" alt="Icono Seguridad" loading="lazy">
-                <h3>Seguridad</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium saepe repellat, sequi quidem
-                    repudiandae quasi cupiditate, corporis tempore quos quam exercitationem, reiciendis illo dolor
-                    commodi. Eos aliquid ad debitis ut?
-                </p>
-            </div>
-            <div class="icono">
-                <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
-                <h3>Precio</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium saepe repellat, sequi quidem
-                    repudiandae quasi cupiditate, corporis tempore quos quam exercitationem, reiciendis illo dolor
-                    commodi. Eos aliquid ad debitis ut?
-                </p>
-            </div>
-            <div class="icono">
-                <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
-                <h3>A Tiempo</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium saepe repellat, sequi quidem
-                    repudiandae quasi cupiditate, corporis tempore quos quam exercitationem, reiciendis illo dolor
-                    commodi. Eos aliquid ad debitis ut?
-                </p>
-            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aspernatur laborum ipsam, eum expedita
+                recusandae esse suscipit possimus ipsa quo velit, incidunt ratione, perspiciatis iusto dolorum
+                tenetur sunt delectus quibusdam.
+            </p>
         </div>
-    </section>
+    </div>
+</main>
+
+<section class="contenedor seccion">
+    <h1>Más sobre nosotros</h1>
+    <div class="iconos-nosotros">
+        <div class="icono">
+            <img src="build/img/icono1.svg" alt="Icono Seguridad" loading="lazy">
+            <h3>Seguridad</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium saepe repellat, sequi quidem
+                repudiandae quasi cupiditate, corporis tempore quos quam exercitationem, reiciendis illo dolor
+                commodi. Eos aliquid ad debitis ut?
+            </p>
+        </div>
+        <div class="icono">
+            <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
+            <h3>Precio</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium saepe repellat, sequi quidem
+                repudiandae quasi cupiditate, corporis tempore quos quam exercitationem, reiciendis illo dolor
+                commodi. Eos aliquid ad debitis ut?
+            </p>
+        </div>
+        <div class="icono">
+            <img src="build/img/icono3.svg" alt="Icono Tiempo" loading="lazy">
+            <h3>A Tiempo</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Praesentium saepe repellat, sequi quidem
+                repudiandae quasi cupiditate, corporis tempore quos quam exercitationem, reiciendis illo dolor
+                commodi. Eos aliquid ad debitis ut?
+            </p>
+        </div>
+    </div>
+</section>
 
 
 
 
-    <?php
-        incluirTemplate('footer');
-    ?>
+<?php
+incluirTemplate('footer');
+?>
