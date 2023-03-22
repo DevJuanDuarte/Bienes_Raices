65d497201a4406ea30dfe733162a8a4de0d8c6b8
DevJuanDuarte <juan.devcontact@gmail.com>
2023-03-22 10:48:30 -0500

    Almacenando en la base de datos

--- classes/Propiedad.php
+++ classes/Propiedad.php
@@ -47,17 +47,17 @@ class Propiedad {
 
         //Sanitizar los atributos antes de insertar:
         $atributos = $this->sanitizarAtributos();
-        debuguear($atributos);
 
 
         //Insertar en la base de datos:
-        $query = "INSERT INTO propiedades (titulo, precio, imagen, descripcion,
-         habitaciones, wc, estacionamiento, creado, vendedorId)
-        VALUES ('$this->titulo', '$this->precio', '$this->imagen', 
-        '$this->descripcion', '$this->habitaciones', '$$this->wc', 
-        '$this->estacionamiento', '$this->creado',  '$this->vendedorId')";
+        $query = "INSERT INTO propiedades ( ";
+        $query .= join(', ', array_keys($atributos));
+        $query .= " ) VALUES (' "; 
+        $query .= join("', '", array_values($atributos));
+        $query .= " ') ";
 
         $resultado = self::$db->query($query);
+
         debuguear($resultado);
     }
 
